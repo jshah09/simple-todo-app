aae17d6d5b57322a6db074576bcb4910bfa8987d
jshah09 <jshah@wiley.com>
2020-09-23 12:52:14 +0530

    Updates

---
+++ precache-manifest.a6a7ac46c9f5520c39aa26f5fa2fe94f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf40c2f11ac6a8ae065ddce463ded57",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "2d007004e638831f4995",
+    "url": "/simple-todo-app/static/css/main.5d3ee447.chunk.css"
+  },
+  {
+    "revision": "445c595ca68398fa1075",
+    "url": "/simple-todo-app/static/js/2.ca5d98c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.ca5d98c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d007004e638831f4995",
+    "url": "/simple-todo-app/static/js/main.48087ea7.chunk.js"
+  },
+  {
+    "revision": "201b5cccffcae2a0c4ce",
+    "url": "/simple-todo-app/static/js/runtime-main.244ca8d7.js"
+  }
+]);
